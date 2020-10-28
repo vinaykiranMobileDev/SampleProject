29840b19bf20047cab1788cabedf3ec0b8caac75
vinaykiran <vinay.kiran1993@gmail.com>
2020-10-28 09:58:20 +0530

    Commit 1 New Project created

---
+++ SampleProject/SampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by VinayKiran M on 28/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .orange
+    }
+
+
+}
+
