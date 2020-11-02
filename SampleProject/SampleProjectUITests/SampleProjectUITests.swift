b2e9441caaa00fcc46001ff51a53e3998dbae4e1
vinay Kiran <vinay1993kiran@gmail.com>
2020-11-02 14:48:44 +0530

    Bug Fix

--- SampleProject/SampleProjectUITests/SampleProjectUITests.swift
+++ SampleProject/SampleProjectUITests/SampleProjectUITests.swift
@@ -34,7 +34,7 @@ class SampleProjectUITests: XCTestCase {
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
                 XCUIApplication().launch()
             }
         }
